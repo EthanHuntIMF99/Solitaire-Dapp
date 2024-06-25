58230cbc10079e067ab499d81a1e6127e3d23543
EthanHuntIMF99 <142003515+EthanHuntIMF99@users.noreply.github.com>
2024-06-25 13:27:26 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@
 //   port: process.env.PORT || 5000,
 // };
 module.exports = {
-  dbURI: process.env.MONGODB_URI,
+  dbURI: process.env.MONGODB_URI || "mongodb://localhost:27017/solitaire",
   discordClientId: process.env.DISCORD_CLIENT_ID,
   discordClientSecret: process.env.DISCORD_CLIENT_SECRET,
   discordRedirectUri: process.env.DISCORD_REDIRECT_URI,
