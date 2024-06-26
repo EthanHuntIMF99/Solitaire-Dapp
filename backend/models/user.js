7e5d5ce802715abb36c5ba159f07f012636a5276
EthanHuntIMF99 <142003515+EthanHuntIMF99@users.noreply.github.com>
2024-06-26 10:24:35 +0530

    Update user.js
    to save user details like id and username

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,15 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+
+router.get('/login', authController.login);
+router.get('/callback', authController.callback);
+router.post('/connectWallet', authController.connectWallet);
+router.post('/register', authController.register);
+
+module.exports = router;
 
 const UserSchema = new mongoose.Schema({
   discordId: String,
