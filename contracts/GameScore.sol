87e2ea5cfed2e27ca19a803e3b5e97125d044c21
EthanHuntIMF99 <142003515+EthanHuntIMF99@users.noreply.github.com>
2024-06-26 20:11:06 +0530

    Update GameScore.sol
    added the variable 'movesdone'

--- contracts/GameScore.sol
+++ contracts/GameScore.sol
@@ -3,15 +3,16 @@ pragma solidity 0.8.19;
 
 contract GameScores {
     struct Score {
-        uint256 startTime;
-        uint256 endTime;
-        uint256 score;
+        uint64 startTime;
+        uint64 endTime;
+        uint64 score;
+        uint64 movesDone;
     }
 
     mapping(address => Score[]) public scores;
 
-    function addScore(uint256 _startTime, uint256 _endTime, uint256 _score) public {
-        scores[msg.sender].push(Score(_startTime, _endTime, _score));
+    function addScore(uint64 _startTime, uint64 _endTime, uint64 _score, uint64 _movesDone) public {
+        scores[msg.sender].push(Score(_startTime, _endTime, _score, _movesDone));
     }
 
     function getScores(address _player) public view returns (Score[] memory) {
