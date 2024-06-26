6dac99e98e15b0e940dfa050e367c73a5c0da4f8
EthanHuntIMF99 <142003515+EthanHuntIMF99@users.noreply.github.com>
2024-06-26 20:10:04 +0530

    Update gameScore.js
    added functionality for 'movesdone' variable

--- backend/models/gameScore.js
+++ backend/models/gameScore.js
@@ -4,7 +4,9 @@ const GameScoreSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   score: { type: Number, required: true },
   startTime: { type: Date, required: true },
-  endTime: { type: Date, required: true }
+  endTime: { type: Date, required: true },
+  timeTaken: { type: Number, required: true },
+  movesDone: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('GameScore', GameScoreSchema);
